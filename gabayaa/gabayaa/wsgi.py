635179c728fb45d69c54c78cbaf88a9a351410ce
Gamme99 <galmogalamso@gmail.com>
2025-08-09 15:47:01 -0700

    fixed directory structure handling in render.yaml

--- gabayaa/gabayaa/wsgi.py
+++ gabayaa/gabayaa/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gabayaa.gabayaa.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gabayaa.settings')
 
 application = get_wsgi_application()
