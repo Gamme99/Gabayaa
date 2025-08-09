e3d7ea5a420572d18fbd6d98dabea420eb6b4def
Gamme99 <galmogalamso@gmail.com>
2025-08-09 15:40:06 -0700

    set DJANGO_SETTING_MODULE to gabayaa.gabayaa.settings in wsgi asgi and manage files

--- gabayaa/manage.py
+++ gabayaa/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gabayaa.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gabayaa.gabayaa.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
